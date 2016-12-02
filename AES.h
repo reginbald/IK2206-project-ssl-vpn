42b4c46a7c6167c680b8716010cea834e796c4d5
einarlogi <einarlogi14@gmail.com>
2016-12-02 13:29:53 +0100

    initial decrypt for AES

--- AES.h
+++ AES.h
@@ -1,4 +1,5 @@
 void encrypt(char *plaintext, int plaintext_len, char *iv, 
     char *ciphertext);
 
-void decrypt();
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+  unsigned char *iv, unsigned char *plaintext);
