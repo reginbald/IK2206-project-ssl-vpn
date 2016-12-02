f38ea5b1a3bbc3db7a91730713cab5833205df14
Maxime Dufour <62maxime@n188-p250.kthopen.kth.se>
2016-12-02 13:30:26 +0100

    implementation of AES

--- AES.c
+++ AES.c
@@ -3,13 +3,51 @@
 #include <openssl/err.h>
 #include <string.h>
 
+/* Require:
+    - Key : 256 bit long
+    - IV : 128 bit long
+*/
+int encrypt(unsigned char *plaintext, int plaintext_len, 
+    unsigned char *key, unsigned char *iv, unsigned char *ciphertext) {
+    // Variables
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int ciphertext_len;
 
-void encrypt(char *plaintext, int plaintext_len, char *iv, 
-    char *ciphertext) {
+    //Initialization of the Cipher context
+    if (!(ctx = EVP_CIPHER_CTX_new())) {
+        perror("Init Cipher Context");
+        exit(1);
+    }
 
+    //Init of encryption cipher
+    if (!(EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))) {
+        perror("Init Encryption Cipher");
+        exit(1);
+    }
+
+    //Compute the encryption
+    if (!(EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))) {
+        perror("Computation AES");
+        exit(1);
+    }
+    ciphertext_len = len;
+
+    //Finalize the encryption
+    if (!(EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))) {
+        perror("Finalization AES");
+        exit(1);
+    }
+    ciphertext_len += len;
+
+    //Clean Up
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
 }
 
 void decrypt() {
 
 }
+
 
--- AES.h
+++ AES.h
@@ -1,4 +1,9 @@
-void encrypt(char *plaintext, int plaintext_len, char *iv, 
-    char *ciphertext);
+#ifndef __AES_H__
+#define __AES_H__
+
+int encrypt(unsigned char *plaintext, int plaintext_len, 
+    unsigned char *key, unsigned char *iv, unsigned char *ciphertext);
 
 void decrypt();
+
+#endif
