dbe3ba04d8b891c72d6d74d6c6bfa2663b31c1cf
reginbald <mail@reginbald.com>
2016-12-03 12:30:13 +0100

    script input eth#

--- scripts/computer1-host1.sh
+++ scripts/computer1-host1.sh
@@ -1,5 +1,7 @@
 # Run this on host1
-sudo ifconfig eth13 10.0.20.100
+echo -n "Type eth# and hit [Enter] > "
+read eth
+sudo ifconfig $eth 10.0.20.100
 sudo route add -net 10.0.30.0 netmask 255.255.255.0 gw 10.0.20.1
 sudo route add -host 10.0.5.10 gw 10.0.20.1
 
